d638287f287c4df2ee4e629804756571bdfe9c9d
rongyi <rongyi@onchain.com>
2023-08-19 10:50:05 +0800

    Save

--- craftinginterpreters-rs/src/main.rs
+++ craftinginterpreters-rs/src/main.rs
@@ -8,13 +8,13 @@ type Value = f64;
 
 #[derive(Debug, Clone, Copy)]
 enum Instruction {
-    Negate,
     Add,
-    Substract,
-    Multiply,
-    Divide,
     Constant(usize),
+    Divide,
+    Multiply,
+    Negate,
     Return,
+    Substract,
 }
 
 struct Chunk {
@@ -62,11 +62,11 @@ impl Chunk {
             print!("{:>4} ", line);
         }
         match instruction {
-            Instruction::Negate => println!("OP_NEGATE"),
             Instruction::Add => println!("OP_ADD"),
-            Instruction::Substract => println!("OP_SUBSTRACT"),
+            Instruction::Divide => println!("OP_DIVIDE"),
             Instruction::Multiply => println!("OP_MULTIPLY"),
-            Instruction::Divide => println!("OP_DVIDE"),
+            Instruction::Negate => println!("OP_NEGATE"),
+            Instruction::Substract => println!("OP_SUBSTRACT"),
             Instruction::Return => println!("OP_RETURN"),
             Instruction::Constant(index) => {
                 let value = self.constants[*index];
@@ -221,12 +221,12 @@ impl<'a> Scanner<'a> {
         }
     }
     fn scan_token(&mut self) -> Token {
+        self.skip_whitespace();
         self.start = self.current;
         if self.is_at_end() {
             return self.make_token(TokenType::Eof);
         }
-        let c = self.advance();
-        match c {
+        match self.advance() {
             b'(' => self.make_token(TokenType::LeftParen),
             b')' => self.make_token(TokenType::RightParen),
             b'{' => self.make_token(TokenType::LeftBrace),
@@ -246,12 +246,15 @@ impl<'a> Scanner<'a> {
             b'<' => self.make_token(TokenType::Less),
             b'>' if self.matches(b'=') => self.make_token(TokenType::GreaterEqual),
             b'>' => self.make_token(TokenType::Greater),
+            b'"' => self.string(),
             _ => self.error_token("Unexpected character."),
         }
     }
+
     fn is_at_end(&self) -> bool {
-        self.current >= self.code.len()
+        return self.current == self.code.len() - 1;
     }
+
     fn make_token(&self, kind: TokenType) -> Token {
         Token {
             kind,
@@ -259,6 +262,19 @@ impl<'a> Scanner<'a> {
             line: self.line,
         }
     }
+
+    fn peek(&self) -> u8 {
+        self.code.as_bytes()[self.current]
+    }
+
+    fn peek_next(&self) -> u8 {
+        if self.is_at_end() {
+            b'\0'
+        } else {
+            self.code.as_bytes()[self.current + 1]
+        }
+    }
+
     fn error_token(&self, message: &'static str) -> Token {
         Token {
             kind: TokenType::Error,
@@ -268,7 +284,7 @@ impl<'a> Scanner<'a> {
     }
 
     fn advance(&mut self) -> u8 {
-        let c = self.code.as_bytes()[self.current];
+        let c = self.peek();
         self.current += 1;
 
         c
@@ -277,11 +293,45 @@ impl<'a> Scanner<'a> {
     fn matches(&mut self, expected: u8) -> bool {
         if self.is_at_end() {
             false
-        } else if self.code.as_bytes()[self.current] != expected {
+        } else if self.peek() != expected {
             false
         } else {
             self.current += 1;
             true
+        }
+    }
+
+    fn skip_whitespace(&mut self) {
+        loop {
+            match self.peek() {
+                b' ' | b'\r' | b'\t' => {
+                    self.advance();
+                }
+                b'\n' => {
+                    self.line += 1;
+                    self.advance();
+                }
+                // comment, consume this line
+                b'/' if self.peek_next() == b'/' => {
+                    while self.peek() != b'\n' && !self.is_at_end() {
+                        self.advance();
+                    }
+                }
+                _ => return,
+            }
+        }
+    }
+
+    // string literal
+    fn string(&mut self) -> Token {
+        while self.peek() != b'"' && !self.is_at_end() {
+            self.advance();
+        }
+        if self.is_at_end() {
+            self.error_token("Unterminated string.")
+        } else {
+            self.advance();
+            self.make_token(TokenType::String)
         }
     }
 }
