5e8ef8db358e5c07b9d6ea747579ec10250fe10f
rongyi <rongyi@onchain.com>
2023-08-19 10:26:25 +0800

    Save

--- craftinginterpreters-rs/src/main.rs
+++ craftinginterpreters-rs/src/main.rs
@@ -1,6 +1,7 @@
 use std::env;
 use std::fs;
 use std::io::{self, Write};
+use std::process;
 
 const DEBUG: bool = true;
 type Value = f64;
@@ -41,6 +42,10 @@ impl Chunk {
         self.constants.len() - 1
     }
 
+    fn read_constant(&self, index: usize) -> Value {
+        self.constants[index]
+    }
+
     fn disassemble(&self, name: &str) {
         println!("=== {} ===", name);
         for (offset, instruction) in self.code.iter().enumerate() {
@@ -71,7 +76,7 @@ impl Chunk {
     }
 }
 
-enum InterpreResult {
+enum InterpreterResult {
     Ok,
     CompileError,
     RuntimeError,
@@ -103,13 +108,7 @@ impl Vm {
         self.push(f(a, b));
     }
 
-    fn next_instruction(&mut self) -> Instruction {
-        let instruction = self.chunk.code[self.ip];
-        self.ip += 1;
-        instruction
-    }
-
-    fn run(&mut self) -> InterpreResult {
+    fn run(&mut self) -> InterpreterResult {
         loop {
             for val in self.stack.iter() {
                 print!("[ {} ]", val);
@@ -122,7 +121,7 @@ impl Vm {
             }
             match instruction {
                 Instruction::Constant(index) => {
-                    let val = self.chunk.constants[index];
+                    let val = self.chunk.read_constant(index);
                     self.stack.push(val);
                 }
                 Instruction::Negate => {
@@ -131,24 +130,173 @@ impl Vm {
                 }
                 Instruction::Return => {
                     println!("{}", self.stack.pop().expect("empty stack!"));
-                    return InterpreResult::Ok;
+                    return InterpreterResult::Ok;
                 }
                 Instruction::Add => self.binary_op(|a, b| a + b),
-                Instruction::Substract => self.binary_op(|a, b| a - b),
-                Instruction::Multiply => self.binary_op(|a, b| a * b),
                 Instruction::Divide => self.binary_op(|a, b| a / b),
+                Instruction::Multiply => self.binary_op(|a, b| a * b),
+                Instruction::Substract => self.binary_op(|a, b| a - b),
             }
+        }
+    }
+
+    fn next_instruction(&mut self) -> Instruction {
+        let instruction = self.chunk.code[self.ip];
+        self.ip += 1;
+        instruction
+    }
+}
+
+#[derive(Debug, Clone, Copy)]
+enum TokenType {
+    LeftParen,
+    RightParen,
+    LeftBrace,
+    RightBrace,
+    Comma,
+    Dot,
+    Minus,
+    Plus,
+    Semicolon,
+    Slash,
+    Star,
+
+    // One or two character tokens.
+    Bang,
+    BangEqual,
+    Equal,
+    EqualEqual,
+    Greater,
+    GreaterEqual,
+    Less,
+    LessEqual,
+
+    // Literals.
+    Identifier,
+    String,
+    Number,
+
+    // Keywords.
+    And,
+    Class,
+    Else,
+    False,
+    For,
+    Fun,
+    If,
+    Nil,
+    Or,
+    Print,
+    Return,
+    Super,
+    This,
+    True,
+    Var,
+    While,
+
+    Error,
+    Eof,
+}
+
+struct Token<'a> {
+    kind: TokenType,
+    line: usize,
+    lexeme: &'a str,
+}
+
+struct Scanner<'a> {
+    code: &'a str,
+    start: usize,
+    current: usize,
+    line: usize,
+}
+
+impl<'a> Scanner<'a> {
+    fn new(code: &'a str) -> Scanner {
+        Scanner {
+            code,
+            start: 0,
+            current: 0,
+            line: 1, // start line 1
+        }
+    }
+    fn scan_token(&mut self) -> Token {
+        self.start = self.current;
+        if self.is_at_end() {
+            return self.make_token(TokenType::Eof);
+        }
+        let c = self.advance();
+        match c {
+            b'(' => self.make_token(TokenType::LeftParen),
+            b')' => self.make_token(TokenType::RightParen),
+            b'{' => self.make_token(TokenType::LeftBrace),
+            b'}' => self.make_token(TokenType::RightBrace),
+            b';' => self.make_token(TokenType::Semicolon),
+            b',' => self.make_token(TokenType::Comma),
+            b'.' => self.make_token(TokenType::Dot),
+            b'-' => self.make_token(TokenType::Minus),
+            b'+' => self.make_token(TokenType::Plus),
+            b'/' => self.make_token(TokenType::Slash),
+            b'*' => self.make_token(TokenType::Star),
+            b'!' if self.matches(b'=') => self.make_token(TokenType::BangEqual),
+            b'!' => self.make_token(TokenType::Bang),
+            b'=' if self.matches(b'=') => self.make_token(TokenType::EqualEqual),
+            b'=' => self.make_token(TokenType::Equal),
+            b'<' if self.matches(b'=') => self.make_token(TokenType::LessEqual),
+            b'<' => self.make_token(TokenType::Less),
+            b'>' if self.matches(b'=') => self.make_token(TokenType::GreaterEqual),
+            b'>' => self.make_token(TokenType::Greater),
+            _ => self.error_token("Unexpected character."),
+        }
+    }
+    fn is_at_end(&self) -> bool {
+        self.current >= self.code.len()
+    }
+    fn make_token(&self, kind: TokenType) -> Token {
+        Token {
+            kind,
+            lexeme: &self.code[self.start..self.current],
+            line: self.line,
+        }
+    }
+    fn error_token(&self, message: &'static str) -> Token {
+        Token {
+            kind: TokenType::Error,
+            lexeme: message,
+            line: self.line,
+        }
+    }
+
+    fn advance(&mut self) -> u8 {
+        let c = self.code.as_bytes()[self.current];
+        self.current += 1;
+
+        c
+    }
+    // if true, advance
+    fn matches(&mut self, expected: u8) -> bool {
+        if self.is_at_end() {
+            false
+        } else if self.code.as_bytes()[self.current] != expected {
+            false
+        } else {
+            self.current += 1;
+            true
         }
     }
 }
 
-fn interpret(chunk: Chunk) -> InterpreResult {
+fn interpret(chunk: Chunk) -> InterpreterResult {
     let mut vm = Vm::new(chunk);
     return vm.run();
 }
 
-fn run_code(code: &str) {
-    println!("Running {}", code);
+fn compile(code: &str) {}
+
+fn run_code(code: &str) -> InterpreterResult {
+    compile(code);
+
+    InterpreterResult::Ok
 }
 
 fn repl() {
@@ -172,10 +320,13 @@ fn run_file(path: &str) {
         Ok(content) => content,
         Err(err) => {
             eprint!("Unable to read file {}: {}", path, err);
-            std::process::exit(1);
+            process::exit(74);
         }
     };
-    run_code(&code);
+    match run_code(&code) {
+        InterpreterResult::Ok => process::exit(65),
+        _ => process::exit(70),
+    }
 }
 
 fn main() {
@@ -183,6 +334,6 @@ fn main() {
     match args.len() {
         1 => repl(),
         2 => run_file(&args[1]),
-        _ => std::process::exit(1),
+        _ => process::exit(64),
     }
 }
