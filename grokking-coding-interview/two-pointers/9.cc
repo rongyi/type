64d3e40cf9e19379de466a77ead1ee75514e46e8
rongyi <rongyi@onchain.com>
2023-09-03 10:21:33 +0800

    Update

--- grokking-coding-interview/two-pointers/9.cc
+++ grokking-coding-interview/two-pointers/9.cc
@@ -5,19 +5,65 @@
 #include <vector>
 using namespace std;
 
+// Given an array of unsorted numbers and a target number, find all unique
+// quadruplets in it, whose sum is equal to the target number.
 class Solution {
  public:
-  void sort(vector<int> &nums) {
+  vector<vector<int>> four_sum(vector<int> &nums, int target) {
+    sort(nums.begin(), nums.end());
+    int sz = nums.size();
+    vector<vector<int>> ret;
+    for (int i = 0; i < sz - 3; i++) {
+      // dedup
+      if (i > 0 && nums[i] == nums[i - 1]) {
+        continue;
+      }
+      for (int j = i + 1; j < sz - 2; j++) {
+        if (j > i + 1 && nums[j] == nums[j - 1]) {
+          continue;
+        }
+        two_sum(nums, i, j, target, j + 1, sz - 1, ret);
+      }
+    }
+    return ret;
+  }
 
+ private:
+  void two_sum(vector<int> &nums, int i, int j, int target, int left, int right,
+               vector<vector<int>> &ret) {
+    while (left < right) {
+      int cur_sum = nums[i] + nums[j] + nums[left] + nums[right];
+      if (cur_sum == target) {
+        ret.push_back({nums[i], nums[j], nums[left], nums[right]});
+        left++;
+        right--;
+        // dedup
+        while (left < right && nums[left] == nums[left - 1]) {
+          left++;
+        }
+        while (left < right && nums[right] == nums[right + 1]) {
+          right--;
+        }
+      } else if (cur_sum < target) {
+        // too small, need to get bigger
+        left++;
+      } else {
+        // too big, shrink the big value
+        right--;
+      }
+    }
   }
 };
 
 int main() {
   Solution so;
-  vector<int> input{1, 0, 2, 1, 0};
-  so.sort(input);
-  for (auto &num : input) {
-    cout << num << " ";
+  vector<int> input{4, 1, 2, -1, 1, -3};
+  auto numss = so.four_sum(input, 1);
+  for (auto &nums : numss) {
+    for (auto &num : nums) {
+      cout << num << " ";
+    }
+    cout << endl;
   }
   cout << endl;
 }
