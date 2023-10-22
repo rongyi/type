948d0d31e414779a120136ffec544287aa57f187
rongyi <rongyi@onchain.com>
2023-10-22 19:35:23 +0800

    Update

--- grokking-coding-interview/cylicsort/1.cc
+++ grokking-coding-interview/cylicsort/1.cc
@@ -26,7 +26,7 @@ class MissingOne {
     int sz = nums.size();
     int i = 0;
     while (i < sz) {
-      if (nums[i] < sz && i != nums[i]) {
+      if (nums[i] < sz && nums[i] != nums[nums[i]]) {
         swap(nums[i], nums[nums[i]]);
       } else {
         i++;
@@ -42,6 +42,30 @@ class MissingOne {
   }
 };
 
+class AllMissingNumbers {
+ public:
+  static vector<int> allMissingNumber(vector<int> &nums) {
+    int sz = nums.size();
+    int i = 0;
+    while (i < sz) {
+      // current value should at postion: expect_pos
+      if (nums[i] != nums[nums[i] - 1]) {
+        swap(nums[i], nums[nums[i] - 1]);
+      } else {
+        i++;
+      }
+    }
+    vector<int> ret;
+    for (int i = 0; i < sz; i++) {
+      if (nums[i] != i + 1) {
+        ret.push_back(i + 1);
+      }
+    }
+
+    return ret;
+  }
+};
+
 int main() {
   // vector<int> input = {3, 1, 5, 4, 2};
   // CylicSort::sort(input);
@@ -49,9 +73,15 @@ int main() {
   // cout << num << endl;
   //}
   //
-  vector<int> input{4, 0, 3, 1};
-  cout << MissingOne::findMissing(input) << endl;
-  input = {8, 3, 5, 2, 4, 6, 0, 1};
-  cout << MissingOne::findMissing(input) << endl;
+  // vector<int> input{4, 0, 3, 1};
+  // cout << MissingOne::findMissing(input) << endl;
+  // input = {8, 3, 5, 2, 4, 6, 0, 1};
+  // cout << MissingOne::findMissing(input) << endl;
+  //
+  vector<int> input{2, 3, 1, 8, 2, 3, 5, 1};
+  auto missing = AllMissingNumbers::allMissingNumber(input);
+  for (auto m : missing) {
+    cout << m << endl;
+  }
 }
 
