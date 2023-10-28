fbfc40cffc7ecbd6ce1d9fc6efc97bc8e24957d9
rongyi <rongyi@onchain.com>
2023-10-28 18:33:59 +0800

    Ac

--- luogu/weapon.cc
+++ luogu/weapon.cc
@@ -19,8 +19,8 @@ int op_sz = 0;
 int ko = 0;
 using GridValue = array<int, 3>;
 vector<vector<GridValue>> grid =
-    vector<vector<GridValue>>(101, vector<GridValue>(101));
-vector<vector<int>> visited = vector<vector<int>>(101, vector<int>(101, 0));
+    vector<vector<GridValue>>(105, vector<GridValue>(105));
+vector<vector<int>> visited = vector<vector<int>>(105, vector<int>(105, 0));
 // 霰弹枪>突击步枪>射手步枪>栓动步枪>空手
 array<int, 10> gun_priority = {0, 3, 3, 2, 1, 3, 2, 2, 4, 4};
 
@@ -44,65 +44,73 @@ void switch_weapon() {
   int newl = gun_priority[grid[x][y][0]];
   if (newl > l1) {
     main_weapon = grid[x][y][0];
-  } else if (newl != l1 && newl > l2) {
+  } else if (newl < l1 && newl > l2) {
     sec_weapon = grid[x][y][0];
   }
 }
 
 // id count
 void update_inventory() {
-  int id = grid[x][y][0];
-  int num = grid[x][y][1];
-  int need_space = space_unit[id] * num;
+  int need_space = space_unit[grid[x][y][0]] * grid[x][y][1];
+  int id1 = gun_bullet[main_weapon];
+  int id2 = gun_bullet[sec_weapon];
+  // pickup directly
   if (cur_sz + need_space <= pack_sz) {
     cur_sz += need_space;
-    package[id].first += num;
-    package[id].second = timestamp;
+    package[grid[x][y][0]].first += grid[x][y][1];
+    package[grid[x][y][0]].second = timestamp;
     return;
   }
 
-  int cur_ts = numeric_limits<int>::max();
-  int id1 = gun_bullet[main_weapon];
-  int id2 = gun_bullet[sec_weapon];
-  int delete_id = -1;
-
-  vector<int> ids(17, 0);
-  iota(ids.begin(), ids.end(), 0);
-  sort(ids.begin(), ids.end(),
-       [&](auto &l, auto &r) { return package[l].second < package[r].second; });
-  for (auto &id : ids) {
-    if (cur_sz + need_space <= pack_sz) {
-      break;
-    }
-    if (package[id].first == 0 || id == id1 || id == id2) {
-      continue;
-    }
-    int drop_num =
-        (cur_sz + need_space - pack_sz + space_unit[id] - 1) / space_unit[id];
-    drop_num = min(drop_num, package[id].first);
-    cur_sz -= drop_num * space_unit[id];
-    package[id].first -= drop_num;
-  }
-
-  if (cur_sz + need_space > pack_sz) {
-    for (auto &id : ids) {
-      if (cur_sz + need_space <= pack_sz) {
-        break;
+  int flag1 = 1;
+  int not_found = 0;
+  int min_ts = 1e9;
+  int del_id = 0;
+  while (cur_sz + need_space > pack_sz) {
+    if (flag1) {
+      not_found = true;
+      min_ts = 1e9;
+      for (int i = 14; i <= 16; i++) {
+        if (package[i].first && i != id1 && i != id2) {
+          if (package[i].second < min_ts) {
+            min_ts = package[i].second;
+            del_id = i;
+            not_found = false;
+          }
+        }
       }
-      if (package[id].first == 0) {
+      if (not_found) {
+        flag1 = 0;
         continue;
       }
-      int drop_num =
-          (cur_sz + need_space - pack_sz + space_unit[id] - 1) / space_unit[id];
-      drop_num = min(drop_num, package[id].first);
-      cur_sz -= drop_num * space_unit[id];
-      package[id].first -= drop_num;
+      int drop_num = (cur_sz + need_space - pack_sz + space_unit[del_id] - 1) /
+                     space_unit[del_id];
+      drop_num =
+          drop_num < package[del_id].first ? drop_num : package[del_id].first;
+      cur_sz -= space_unit[del_id] * drop_num;
+      package[del_id].first -= drop_num;
+    } else {
+      min_ts = 1e9;
+      for (int i = 10; i <= 16; i++) {
+        if (package[i].first) {
+          if (package[i].second < min_ts) {
+            min_ts = package[i].second;
+            del_id = i;
+          }
+        }
+      }
+
+      int drop_num = (cur_sz + need_space - pack_sz + space_unit[del_id] - 1) /
+                     space_unit[del_id];
+      drop_num =
+          drop_num < package[del_id].first ? drop_num : package[del_id].first;
+      cur_sz -= space_unit[del_id] * drop_num;
+      package[del_id].first -= drop_num;
     }
   }
-
   cur_sz += need_space;
-  package[id].first += num;
-  package[id].second = timestamp;
+  package[grid[x][y][0]].first += grid[x][y][1];
+  package[grid[x][y][0]].second = timestamp;
 }
 
 void fight() {
@@ -143,8 +151,8 @@ int main() {
   cin >> m >> n >> pack_sz >> op_sz;
   pack_sz *= 10;
 
-  for (int i = 0; i < m; i++) {
-    for (int j = 0; j < n; j++) {
+  for (int i = 1; i <= m; i++) {
+    for (int j = 1; j <= n; j++) {
       int first = 0;
       int second = 0;
       cin >> grid[i][j][0] >> grid[i][j][1];
@@ -161,15 +169,17 @@ int main() {
     ops[i] -= 1;
   }
 
+  x = 1;
+  y = 1;
   entry();
 
   for (auto &d : ops) {
     timestamp++;
-    x += dirs[d][0];
-    y += dirs[d][1];
     if (ko) {
       break;
     }
+    x += dirs[d][0];
+    y += dirs[d][1];
     entry();
   }
   if (ko) {
@@ -177,16 +187,17 @@ int main() {
   } else {
     printf("%d\n", main_weapon);
     printf("%d\n", sec_weapon);
-
-    vector<int> ids(17, 0);
-    iota(ids.begin(), ids.end(), 0);
-    sort(ids.begin(), ids.end(), [&](auto &l, auto &r) {
-      return package[l].second < package[r].second;
-    });
-    for (auto id : ids) {
-      if (package[id].first > 0) {
-        printf("%d %d\n", id, package[id].first);
-      }
-    }
+    int pos[7];
+    for (int i = 0; i <= 6; i++) pos[i] = 10 + i;
+    for (int i = 0; i < 6; i++)
+      for (int j = i + 1; j <= 6; j++)
+        if (package[pos[i]].second > package[pos[j]].second) {
+          int tmp = pos[i];
+          pos[i] = pos[j];
+          pos[j] = tmp;
+        }
+    for (int i = 0; i <= 6; i++)
+      if (package[pos[i]].first)
+        printf("%d %d\n", pos[i], package[pos[i]].first);
   }
 }
