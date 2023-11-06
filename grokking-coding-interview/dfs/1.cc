eb8f0bb0d14705352686094eee1c0ebbdd38489a
rongyi <rongyi@onchain.com>
2023-11-07 07:54:31 +0800

    Update code

--- grokking-coding-interview/dfs/1.cc
+++ grokking-coding-interview/dfs/1.cc
@@ -23,8 +23,33 @@ class FindAllTreePaths {
     dfs(root, cur_path, sum, all_paths);
     return all_paths;
   }
+  static int findAnyPathTopDown(TreeNode *root, int t) {
+    vector<int> cur_path;
+    return countPath(root, t, cur_path);
+  }
 
  private:
+  static int countPath(TreeNode *cur, int t, vector<int> &cur_path) {
+    if (!cur) {
+      return 0;
+    }
+    cur_path.push_back(cur->val_);
+    int ret = 0;
+    int psum = 0;
+    for (auto it = cur_path.rbegin(); it != cur_path.rend(); ++it) {
+      psum += *it;
+      if (psum == t) {
+        ret++;
+      }
+    }
+
+    ret += countPath(cur->left_, t, cur_path);
+    ret += countPath(cur->right_, t, cur_path);
+
+    cur_path.pop_back();
+
+    return ret;
+  }
   static void dfs(TreeNode *cur, vector<int> &cur_path, int sum,
                   vector<vector<int>> &all_paths) {
     if (!cur) {
