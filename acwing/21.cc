db7ba598078242843456fb27cb77df7851e82da0
rongyi <rongyi@onchain.com>
2024-05-17 14:50:01 +0800

    Update

--- acwing/21.cc
+++ acwing/21.cc
@@ -1,22 +1,66 @@
 #include "xxx.hpp"
+#include <vector>
 
 class Solution {
 public:
-  int Fibonacci(int n) {
-    if (n == 0) {
-      return 0;
+  bool hasPath(vector<vector<char>> &matrix, string &str) {
+    if (matrix.empty()) {
+      return false;
     }
-    int n1 = 1;
-    int n2 = 1;
-    if (n <= 2) {
-      return 1;
+    int m = matrix.size();
+    if (matrix[0].empty()) {
+      return false;
     }
-    int sum = 0;
-    for (int i = 3; i <= n; i++) {
-      sum = n1 + n2;
-      n1 = n2;
-      n2 = sum;
+    int n = matrix[0].size();
+    for (int i = 0; i < m; i++) {
+      for (int j = 0; j < n; j++) {
+        vector<vector<int>> visited(m, vector<int>(n, 0));
+        visited[i][j] = 1;
+        if (dfs(matrix, visited, str, i, j, 0)) {
+          return true;
+        }
+        // visited[i][j] = 0;
+      }
     }
-    return sum;
+    return false;
   }
+
+  bool dfs(vector<vector<char>> &matrix, vector<vector<int>> &visited,
+           string &s, int x, int y, int idx) {
+    if (s[idx] != matrix[x][y]) {
+      return false;
+    }
+
+    if (idx == s.size() - 1) {
+      return true;
+    }
+
+    for (auto &d : dirs) {
+      auto nx = x + d[0];
+      auto ny = y + d[1];
+      if (nx < 0 || nx >= matrix.size() || ny < 0 || ny >= matrix[0].size() ||
+          visited[nx][ny]) {
+        continue;
+      }
+      visited[nx][ny] = 1;
+      if (dfs(matrix, visited, s, nx, ny, idx + 1)) {
+        return true;
+      }
+      visited[nx][ny] = 0;
+    }
+
+    return false;
+  }
+
+private:
+  vector<vector<int>> dirs = {{0, 1}, {1, 0}, {-1, 0}, {0, -1}};
 };
+
+int main() {
+  Solution so;
+  vector<vector<char>> input{
+      {'A', 'B', 'C', 'E'}, {'S', 'F', 'E', 'S'}, {'A', 'D', 'E', 'E'}};
+  string s{"ABCEFSADEESE"};
+  auto ret = so.hasPath(input, s);
+  cout << ret << endl;
+}
